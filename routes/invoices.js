7cb027f3f84e1719340070cf741a613eefac7d38
Ethan Shen <ethanshen.wu@gmail.com>
2019-03-07 14:33:11 -0800

    GET /invoices/:id to find invoice

--- routes/invoices.js
+++ routes/invoices.js
@@ -10,11 +10,33 @@ router.get('', async function(req, res, next){
     try {
         const results = await db.query(
             `SELECT id, comp_code FROM invoices`);
-        return res.json({invoices: results.rows})
+        return res.json({invoices: results.rows});
     } catch(err) {
-        return next(err)
+        return next(err);
     }
 })
 
+/* GET object on given invoice => 
+{invoice: {id, amt, paid, add_date, paid_date, company: {code, name, description}} */
+router.get('/:id', async function(req, res, next){
+    try {
+        const result = await db.query(
+            `SELECT id, amt, paid, add_date, paid_date, code, name, description
+             FROM invoices 
+             LEFT JOIN companies
+                ON invoices.comp_code = companies.code
+            WHERE id = $1`, [req.params.id]
+        );
+        
+        const {id, amt, paid, add_date, paid_date, code, name, description} = result.rows[0];
+        
+        return res.json({id, amt, paid, add_date, paid_date, company: {code, name, description}});
+
+    } catch(err){
+        err = new ExpressError("Invoice ID could not be found", 404);
+        next(err);
+    }
+
+})
 
 module.exports = router
