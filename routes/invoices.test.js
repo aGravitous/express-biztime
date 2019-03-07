4889218ad5338ad75c993189af4e643024ae15ce
Stéphane Dryefus <floatingrock@gmail.com>
2019-03-07 15:49:53 -0800

    Add boilerplate to test.js files.

--- routes/invoices.test.js
+++ routes/invoices.test.js
@@ -2,4 +2,31 @@ process.env.NODE_ENV = "test";
 
 const app = require("../app");
 const db = require("../db");
+const request = require("supertest");
 const ExpressError = require("../expressError");
+
+let company;
+let invoice;
+
+beforeEach(async function(){
+    let compResult = await db.query(
+        `INSERT INTO companies (code, name, description)
+         VALUES ('Test1', 'TestCompany', 'Small Testing Company')
+         RETURNING code, name, description`);
+    company = compResult.rows[0];
+
+    let invResult = await db.query(
+        `INSERT INTO invoices (comp_code, amt)
+         VALUES ('Test1', 50)
+         RETURNING id, comp_code, amt, paid, add_date, paid_date`);
+    invoices = invResult.rows[0];
+});
+
+afterEach(async function(){
+    await db.query(`DELETE FROM companies`);
+    await db.query(`DELETE FROM invoices`);
+});
+
+afterAll( async function(){
+    await db.end();
+});
