0d5f03001acbdb8ccf62e8aa9b9f8c5cc60c2ac5
Stéphane Dryefus <floatingrock@gmail.com>
2019-03-07 11:07:19 -0800

    Add companies GET route.

--- routes/companies.js
+++ routes/companies.js
@@ -5,6 +5,17 @@ const db = require("../db");
 const router = new express.Router();
 const ExpressError = require("../expressError");
 
+/** GET a list of companies / => [{code, name}, ...] */
 
+router.get('', async (req, res, next) => {
+    try {
+      const results = await db.query(
+          `SELECT code, name FROM companies`
+      );
+      return res.json({companies: results.rows})
+    } catch(err){
+      return next(err)
+    }
+  });
 
 module.exports = router;
