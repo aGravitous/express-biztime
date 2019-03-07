1ee3f2585edaf35c51b39dc0c29019641b33241b
Stéphane Dryefus <floatingrock@gmail.com>
2019-03-07 11:16:10 -0800

    Add GET /companies/:code route.

--- routes/companies.js
+++ routes/companies.js
@@ -10,12 +10,23 @@ const ExpressError = require("../expressError");
 router.get('', async (req, res, next) => {
     try {
       const results = await db.query(
-          `SELECT code, name FROM companies`
-      );
-      return res.json({companies: results.rows})
+          `SELECT code, name FROM companies`);
+      return res.json({companies: results.rows});
     } catch(err){
       return next(err)
     }
   });
 
 module.exports = router;
+
+router.get('/:code', async (req, res, next) => {
+    try {
+        const code = req.params.code
+        const results = await db.query(
+            `SELECT code, name, description FROM companies
+            WHERE code=$1`, [code]);
+        return res.json({company: results.rows[0]});
+    } catch(err){
+        return next(err)
+    }
+});
